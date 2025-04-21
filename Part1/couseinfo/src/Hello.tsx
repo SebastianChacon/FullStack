d3bcf01ea19c0c3d31b7b96dff8374b158f7c9b9
Sebas Trelles <xdmateo90@gmail.com>
2025-04-21 11:53:20 -0500

    courseinfo

--- Part1/couseinfo/src/Hello.tsx
+++
@@ -1,6 +0,0 @@
-const Hello = (props) => {
-  console.log(props);
-  return <h1>Hello {props.name}</h1>;
-};
-
-export default Hello;
---
+++ Part1/couseinfo/src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = (props: any) => {
+  return <h1>{props.course}</h1>;
+};
+
+export default Header;
